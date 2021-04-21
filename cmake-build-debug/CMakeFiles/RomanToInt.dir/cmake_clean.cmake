834040d36630e701659de54b345209166d98b681
zt20001001 <2944334369@qq.com>
2021-04-21 22:04:16 +0800

    111

---
+++ cmake-build-debug/CMakeFiles/RomanToInt.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RomanToInt.dir/cpp/RomanToInt.cpp.obj"
+  "RomanToInt.exe"
+  "RomanToInt.exe.manifest"
+  "RomanToInt.pdb"
+  "libRomanToInt.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RomanToInt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
